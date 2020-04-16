acdbbfc72f54cd4a9a13c15c03716a2eb7109b57
Pedro Reis <pedromlsreis@gmail.com>
2020-04-16 15:18:06 +0100

    Add file for problem 17.

---
+++ problems/problem17/problem17.py
@@ -0,0 +1,22 @@
+# # Problem 17
+
+# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+
+import time
+
+
+def run():
+    solution = 0
+
+    
+    print("solution:", solution)
+
+                       
+if __name__ == "__main__":
+    startTime = time.time()
+    run()
+    print(f"\nThe script took {round(time.time() - startTime, 2)} seconds.")
