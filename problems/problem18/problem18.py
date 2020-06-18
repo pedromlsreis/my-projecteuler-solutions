2b2c8978780f3f5ab3e9a8cdae2fba63da4afbbb
Pedro Reis <pedromlsreis@gmail.com>
2020-06-18 02:36:26 +0100

    Add solution to problem 20

--- problems/problem18/problem18.py
+++ problems/problem18/problem18.py
@@ -1,24 +1,71 @@
-# # Problem 17
+## Problem 18
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
 
-# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
-# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
 
-# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+# That is, 3 + 7 + 4 + 9 = 23.
 
+# Find the maximum total from top to bottom of the triangle below:
+
+# 75
+# 95 64
+# 17 47 82
+# 18 35 87 10
+# 20 04 82 47 65
+# 19 01 23 75 03 34
+# 88 02 77 73 07 63 67
+# 99 65 04 28 06 16 70 92
+# 41 41 26 56 83 40 80 70 33
+# 41 48 72 33 47 32 37 16 94 29
+# 53 71 44 65 25 43 91 52 97 51 14
+# 70 11 33 28 77 73 17 78 39 68 17 57
+# 91 71 52 38 17 14 91 43 58 50 27 29 48
+# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+# NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.
+# However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 
 import time
-from num2words import num2words
-
 
 def run():
-    solution = 0
-    first_num = 1
-    last_num = 1000
+    triangle = """75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
 
-    for n in range(first_num, last_num+1):
-        solution += len(num2words(n, lang="en_GB", to="cardinal").replace("-", "").replace(" ", ""))
+    triangle = [line.split(" ") for line in triangle.split("\n")]
+    result, index = 0, 0
+
+    for line_counter, line in enumerate(triangle[1:]):
+        print(f"line {line_counter+1}/{len(triangle)-1}")
+        
+        if int(line[index]) > int(line[index+1]):
+            result += int(line[index])
+            
+        elif int(line[index]) < int(line[index+1]):
+            result += int(line[index+1])
+            index += 1
+            print(f"index is now {index}")
+            
+        else:
+            print("error...")
     
-    print("solution:", solution)
+    print(f"result: {result}")
 
                        
 if __name__ == "__main__":
---
+++ problems/problem20/problem20.py
@@ -0,0 +1,19 @@
+## Problem 20
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+# Find the sum of the digits in the number 100!
+
+import time, numpy as np
+
+def run(n=100):
+    result = np.array(list(str(np.math.factorial(n))), dtype=np.int8).sum()
+    print(f"result: {result}")
+
+                       
+if __name__ == "__main__":
+    startTime = time.time()
+    run()
+    print(f"\nThe script took {round(time.time() - startTime, 2)} seconds.")
