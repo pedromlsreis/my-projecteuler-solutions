400f1f7d1d2b1cf7f851e86b0dfbc3f0fd71626b
Pedro Reis <pedromlsreis@gmail.com>
2020-12-28 20:35:18 +0000

    Add solution to p34.

---
+++ problems/problem34/problem34.py
@@ -0,0 +1,29 @@
+## Problem 34s
+# 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+# Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+# Note: As 1! = 1 and 2! = 2 are not sums they are not included.
+
+import time
+import sys
+sys.path.append("../..")
+from utils.log import MarkdownLogger
+import math
+
+def run():
+    result = 0
+    for number in range(3, int(1e5)):
+        if sum([math.factorial(int(digit)) for digit in str(number)]) == number:
+            result += number
+    return result
+
+
+if __name__ == "__main__":
+    logger = MarkdownLogger(last_problem=723)
+    problem_id = int(sys.argv[0].split("m")[1].split(".")[0])
+    startTime = time.time()
+    solution = run()
+    duration = round(time.time() - startTime, 5)
+    logger.add_problem(solution, problem_id=problem_id, duration=duration, language="Python")
+    print(f"\nThe solution is {solution} and the script took {round(duration, 2)} seconds.")
