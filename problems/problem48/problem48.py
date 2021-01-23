80fec90da6379ee1f2f28443046309982619b2c7
Pedro Reis <pedromlsreis@gmail.com>
2021-01-23 18:39:41 +0000

    Add solution to p48.

---
+++ problems/problem48/problem48.py
@@ -0,0 +1,30 @@
+## Problem 48
+
+# The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+# Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+import time
+import sys
+sys.path.append("../..")
+from utils.log import MarkdownLogger
+
+
+def run():
+    result = 0
+    last = 1000
+
+    for i in range(1, last + 1):
+        result += i**i
+
+    return str(result)[-10:]
+
+
+if __name__ == "__main__":
+    logger = MarkdownLogger(last_problem=723)
+    problem_id = int(sys.argv[0].split("m")[1].split(".")[0])
+    startTime = time.time()
+    solution = run()
+    duration = round(time.time() - startTime, 5)
+    logger.add_problem(solution, problem_id=problem_id, duration=duration, language="Python")
+    print(f"\nThe solution is {solution} and the script took {round(duration, 2)} seconds.")
