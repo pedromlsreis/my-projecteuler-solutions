a3a0db0ae36d53dc9234d736d0544d4d3b7e2407
Pedro Reis <pedromlsreis@gmail.com>
2021-02-04 09:41:37 +0000

    Improve p15 performance

--- problems/problem15/problem15.py
+++ problems/problem15/problem15.py
@@ -24,12 +24,11 @@ def run():
     return int(math.factorial(2 * last) / (math.factorial(last) * math.factorial(last)))
 
 
-print(run())
-# if __name__ == "__main__":
-#     logger = MarkdownLogger(last_problem=723)
-#     problem_id = int(sys.argv[0].split("m")[1].split(".")[0])
-#     startTime = time.time()
-#     solution = run()
-#     duration = round(time.time() - startTime, 5)
-#     logger.add_problem(solution, problem_id=problem_id, duration=duration, language="Python")
-#     print(f"\nThe script took {round(duration, 2)} seconds.")
+if __name__ == "__main__":
+    logger = MarkdownLogger(last_problem=723)
+    problem_id = int(sys.argv[0].split("m")[1].split(".")[0])
+    startTime = time.time()
+    solution = run()
+    duration = round(time.time() - startTime, 5)
+    logger.add_problem(solution, problem_id=problem_id, duration=duration, language="Python")
+    print(f"\nThe script took {round(duration, 2)} seconds.")
