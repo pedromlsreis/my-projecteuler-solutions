a07b672909bf96a33d6c122dc2a152dcc83290e9
Pedro Reis <pedromlsreis@gmail.com>
2021-02-04 09:40:57 +0000

    Improve p15 performance

--- problems/problem15/problem15.py
+++ problems/problem15/problem15.py
@@ -16,19 +16,20 @@ def run():
     em x e 20 em y. Resta é saber qual a sua posição.
     """
     last = 20
-    for n in range(1, last+1):
-        solution = int(math.factorial(2 * n) / (math.factorial(n) * math.factorial(n)))
-        print(f"{n}x{n}: {solution}")
+    # for n in range(1, last+1):
+    #     solution = int(math.factorial(2 * n) / (math.factorial(n) * math.factorial(n)))
+    #     print(f"{n}x{n}: {solution}")
     
-    print(f"final solution: {solution}")
-    return solution
+    # print(f"final solution: {solution}")
+    return int(math.factorial(2 * last) / (math.factorial(last) * math.factorial(last)))
 
 
-if __name__ == "__main__":
-    logger = MarkdownLogger(last_problem=723)
-    problem_id = int(sys.argv[0].split("m")[1].split(".")[0])
-    startTime = time.time()
-    solution = run()
-    duration = round(time.time() - startTime, 5)
-    logger.add_problem(solution, problem_id=problem_id, duration=duration, language="Python")
-    print(f"\nThe script took {round(duration, 2)} seconds.")
+print(run())
+# if __name__ == "__main__":
+#     logger = MarkdownLogger(last_problem=723)
+#     problem_id = int(sys.argv[0].split("m")[1].split(".")[0])
+#     startTime = time.time()
+#     solution = run()
+#     duration = round(time.time() - startTime, 5)
+#     logger.add_problem(solution, problem_id=problem_id, duration=duration, language="Python")
+#     print(f"\nThe script took {round(duration, 2)} seconds.")
